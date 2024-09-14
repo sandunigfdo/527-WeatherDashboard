d009493a39c0e660b23f62a8da23a81329d9a720
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-14 12:19:24 +1200

    Change default port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const handlebars = require("express-handlebars");
 const app = express();
 
 // Listen port will be loaded from .env file, or use 3000 if
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 // Setup Handlebars
 app.engine("handlebars", handlebars.create({
